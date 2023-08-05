e25e847f3800fbdf7515fc3cd72875829a710979
“DaeunYoon” <daa3230@gmail.com>
2023-08-05 14:43:05 +0200

    feat: added solidity

--- app/src/components/Highlight.tsx
+++ app/src/components/Highlight.tsx
@@ -1,4 +1,6 @@
 import hljs from 'highlight.js'
+import hljsDefineSolidity from 'highlightjs-solidity'
+
 import React, { useEffect, useRef, useMemo } from 'react'
 import 'highlight.js/styles/agate.css'
 
@@ -7,16 +9,7 @@ type HighlightProps = {
   children: React.ReactNode
 }
 
-const links = [
-  {
-    keyword: 'google',
-    url: 'https://www.google.com',
-  },
-  {
-    keyword: 'facebook',
-    url: 'https://www.facebook.com',
-  },
-] as const
+hljsDefineSolidity(hljs)
 
 const Highlight = ({ language, children }) => {
   const codeRef = useRef()
@@ -25,19 +18,6 @@ const Highlight = ({ language, children }) => {
   useEffect(() => {
     // Highlight code
     hljs.highlightElement(codeRef.current)
-
-    // Add a link to the "helloWorld" function keyword
-    const keywordElements = codeRef.current.querySelectorAll('.hljs-keyword')
-
-    keywordElements.forEach((element) => {
-      // TODO: check if the keyword is partially matching -> change it to link
-      if (linkKeywords.includes(element.textContent)) {
-        const link = document.createElement('a')
-        link.href = 'https://example.com' // Replace this URL with your external link
-        link.textContent = element.textContent
-        element.parentNode.replaceChild(link, element)
-      }
-    })
   }, [])
 
   return (
