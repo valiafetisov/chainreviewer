234027544abffb0d1a1ccab35f7aa5f17312a9ab
KirillDogadin-std <kirillsc2@gmail.com>
2023-08-06 12:40:24 +0200

    more sample data and refactor the core logic

--- app/src/pages/api/contract.ts
+++ app/src/pages/api/contract.ts
@@ -2,8 +2,18 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { readFileSync } from 'fs';
 import { join } from 'path'
 import { parse, visit } from '@solidity-parser/parser'
+import { ASTNode, StateVariableDeclaration, VariableDeclaration } from '@solidity-parser/parser/dist/src/ast-types';
 
 const SAMPLE_DIR = join(__dirname, '../../../../../sample_data/DssSpell')
+
+interface AddressInfo {
+    filename: string;
+    loc: ASTNode['loc'];
+    range: ASTNode['range'];
+    source: "variable" | "hardcoded" | "interface" | "public_function" | "external_function" | "private_function" | "state";
+    getAddress: (...args: any[]) => string,
+    parent: ASTNode | undefined,
+}
 
 function getContract() {
     return readFileSync(join(SAMPLE_DIR, 'DssSpell.sol'), 'utf8');
@@ -47,9 +57,97 @@ export default async (_req: NextApiRequest, res: NextApiResponse) => {
     if (!ast) {
         return res.status(500).json({ error: 'Server error' });
     }
-    const hardcodedAddresses: string[] = [];
+    const addresses: AddressInfo[] = [];
     visit(ast, {
-        NumberLiteral: (node) => { isAddress(node.number) ? hardcodedAddresses.push(node.number) : null }
+        NumberLiteral: (node, parent) => {
+            isAddress(node.number) && node.loc ? addresses.push(
+                {
+                    filename: "todo",
+                    loc: node.loc,
+                    range: node.range,
+                    source: "hardcoded",
+                    getAddress: () => node.number,
+                    parent,
+                }
+            ) : null
+        },
+        VariableDeclarationStatement: (node, parent) => {
+            if (node.variables.length !== 1
+                || !node.variables[0]
+                || node.variables[0].type !== 'VariableDeclaration'
+            ) {
+                return;
+            }
+            const variableDeclaration = node.variables[0] as VariableDeclaration;
+            if (variableDeclaration.typeName?.type !== 'ElementaryTypeName'
+                || variableDeclaration.typeName.name !== 'address') {
+                return
+            }
+            addresses.push(
+                {
+                    filename: "todo",
+                    loc: node.loc,
+                    range: node.range,
+                    source: "variable",
+                    getAddress: () => "TODO",
+                    parent,
+                }
+            );
+        },
+        FunctionDefinition: (node, parent) => {
+            if (!node.returnParameters || node.returnParameters.length !== 1) {
+                return
+            }
+            if (node.returnParameters[0].typeName?.type !== 'ElementaryTypeName'
+                || node.returnParameters[0].typeName.name !== 'address'
+               ) {
+                return
+            }
+            if (!node.body && node.visibility === 'external') {
+                addresses.push(
+                    {
+                        filename: "todo",
+                        loc: node.loc,
+                        range: node.range,
+                        source: "interface",
+                        getAddress: () => "TODO",
+                        parent,
+                    }
+                );
+                return
+            }
+            const visibility = node.visibility;
+            if (visibility !== 'public' && visibility !== 'external' && visibility !== 'private') {
+                return
+            }
+            addresses.push(
+                {
+                    filename: "todo",
+                    loc: node.loc,
+                    range: node.range,
+                    source: `${visibility}_function`,
+                    getAddress: () => "TODO",
+                    parent,
+                }
+            )
+        },
+        StateVariableDeclaration: (node, parent) => {
+            if (node.variables.length !== 1
+                || node.variables[0].type !== 'VariableDeclaration'
+                || node.variables[0].typeName?.type !== 'ElementaryTypeName'
+                || node.variables[0].typeName.name !== 'address'
+            ) {
+                return;
+            }
+            addresses.push({
+                filename: "todo",
+                loc: node.loc,
+                range: node.range,
+                source: "state",
+                getAddress: () => "TODO",
+                parent,
+            })
+        },
     })
-    res.status(200).json({ ast, abi: JSON.parse(abi), hardcodedAddresses });
+    res.status(200).json({ ast, abi: JSON.parse(abi), addresses });
 };
