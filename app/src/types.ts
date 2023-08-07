c58b17c7cbaa768959b3997ea9c45a31ea415189
KirillDogadin-std <kirillsc2@gmail.com>
2023-08-07 19:01:08 +0200

    Flatten location

--- app/src/types.ts
+++ app/src/types.ts
@@ -2,8 +2,12 @@ import { ASTNode } from '@solidity-parser/parser/dist/src/ast-types';
 export declare interface AddressInfo {
     contractPath: string;
     contractName: string;
-    loc: ASTNode['loc'];
-    range: ASTNode['range'];
+    locStartLine: number;
+    locStartCol: number;
+    locEndLine: number;
+    locEndCol: number;
+    rangeTo?: number;
+    rangeFrom?: number;
     source: "variable" | "hardcoded" | "interface" | "public_function" | "external_function" | "private_function" | "state";
     getAddress: (...args: any[]) => string,
     parent: ASTNode | undefined,
