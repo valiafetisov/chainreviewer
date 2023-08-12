f79cedd6cd0fe0a60fa4441512cb665f2cd1a6e9
“DaeunYoon” <daa3230@gmail.com>
2023-08-12 21:39:54 +0200

    fix: type

--- app/src/helpers/loadContractLibraries.ts
+++ app/src/helpers/loadContractLibraries.ts
@@ -1,23 +1,32 @@
-import getContractInfo from "./getContractInfo";
-import getPrisma from "./getPrisma";
+import getContractInfo from './getContractInfo'
+import getPrisma from './getPrisma'
+import type { SupportedChain } from '~/types'
 
-export default async function loadContractLibraries(address: string, chain: string) {
-  await getContractInfo(address, chain);
-  const contract = await getPrisma().contract.findFirst({ where: {
-    address,
-    chain
-  }})
-  if (!contract) throw new Error("Contract not found");
-  if (contract.library === '') return {};
+export default async function loadContractLibraries(
+  address: string,
+  chain: SupportedChain
+) {
+  await getContractInfo(address, chain)
+  const contract = await getPrisma().contract.findFirst({
+    where: {
+      address,
+      chain,
+    },
+  })
+
+  if (!contract) throw new Error('Contract not found')
+  if (contract.library === '') return {}
   const libraries = contract.library.split(';')
-  const ret = Object.fromEntries(libraries.map(l => {
-    const [key, val] = l.split(':');
-    return [key, `0x${val}`]
-  }));
+  const ret = Object.fromEntries(
+    libraries.map((l) => {
+      const [key, val] = l.split(':')
+      return [key, `0x${val}`]
+    })
+  )
   await Promise.all(
     Object.values(ret).map(async (v) => {
       return loadContractLibraries(v, chain)
     })
   )
-  return ret;
+  return ret
 }
