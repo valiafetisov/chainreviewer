e5a53abfca799d45ccfe712209684d46afad9910
Valia Fetisov <contact@valiafetisov.com>
2023-08-12 22:51:43 +0200

    fix default links

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -58,18 +58,18 @@ export default function Home() {
           href="/contract/base/0x78a087d713Be963Bf307b18F2Ff8122EF9A63ae9"
         >
           BswapToken on Base
-        </Link>{' '}
+        </Link>,{' '}
         <Link
           className="text-primary underline"
           href="/contract/ethereum/0xE592427A0AEce92De3Edee1F18E0157C05861564"
         >
-          UniswapV3Router.sol
-        </Link>{' '}
+          UniswapV3Router
+        </Link>,{' '}
         <Link
           className="text-primary underline"
           href="/contract/ethereum/0xa1c423ee0bbC927EF5809c7ebB24c86D4284e431"
         >
-          DssSpell.sol on Ethereum
+          MakerDAO DssSpell on Ethereum
         </Link>{' '}
       </p>
     </div>
